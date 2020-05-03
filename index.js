d98d5b7a12bff6315405f3d3f168a52ab1f69552
Robson Carlos <rcs.robson.carlos.santos@gmail.com>
2020-05-03 13:59:51 +0100

    configuracao basica

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const bodyParse = require('body-parser');
+const connection = require('./database/database');
 
 //view engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-app.get("/",(req, res) => {
-    res.render("index");
+//file static
+app.use(express.static('public'));
+
+//body parser
+app.use(bodyParse.urlencoded({ extended: false}));
+app.use(bodyParse.json());
+
+//Datadase
+connection.authenticate().then(() => {
+    console.log('Conexao com sucesso!');
+}).catch((erro) => {
+    console.log('Conexao erro: '+erro);
 });
 
-app.listen("8080",() => {
-    console.log("Servidor rodando!");
+app.get('/',(req, res) => {
+    res.render('index');
+});
+
+app.listen('8080',() => {
+    console.log('Servidor rodando!');
 });
