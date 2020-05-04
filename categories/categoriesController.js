592e1a13d54fa1995926e8eec3318002fb089ee5
Robson Carlos <rcs.robson.carlos.santos@gmail.com>
2020-05-04 21:50:19 +0100

    crud category

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -1,12 +1,81 @@
 const express = require('express');
 const router = express.Router();
+const slugify = require('slugify');
+const modelCategory = require('./category');
 
-router.get('/categories',(req, res) => {
-    res.send('Categories');
+router.get('/admin/categories/new', (req, res) => {
+    res.render('./admin/categories/new');
 });
 
-router.get('/admin/categories',(req, res) => {
-    res.send('Admin Categories');
+router.post('/categories/save', (req, res) => {
+    var title = req.body.title;
+    if (title != undefined) {
+        modelCategory.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect('/admin/categories');
+        });
+    } else {
+        res.redirect('/admin/categories/new');
+    }
+});
+
+router.get('/admin/categories', (req, res) => {
+    modelCategory.findAll().then(categories => {
+        res.render('./admin/categories/index', {
+            categories: categories
+        });
+    });
+});
+
+router.post('/categories/delete', (req, res) => {
+    var id = req.body.idCategory;
+    if (id != undefined) {
+        if (isNaN(id)) {
+            res.redirect('/admin/categories');
+        }
+        modelCategory.destroy({
+            where: { id: id }
+        }).then(() => {
+            res.redirect('/admin/categories');
+        });
+    } else {
+        res.redirect('/admin/categories');
+    }
+});
+
+router.get('/admin/categories/edit/:id', (req, res) => {
+    var id = req.params.id;
+    if (isNaN(id)) {
+        res.redirect('/admin/categories');
+    }
+    console.log('id busca: ' + id);
+    modelCategory.findByPk(id).then(category => {
+        if (category != undefined) {
+            res.render('admin/categories/edit', {
+                category: category
+            });
+        }
+        console.log('entrou /admin/categories');
+        res.redirect('/admin/categories');
+    }).catch(erro => {
+        res.redirect('/admin/categories');
+    });
+});
+
+router.post('/categories/update', (req, res) => {
+    var idCategory = req.body.idCategory;
+    var title = req.body.title;
+    modelCategory.update({
+        title: title,
+        slug: slugify(title)
+    },{
+        where: {id: idCategory}
+    }).then(() => {
+        res.redirect('/admin/categories');
+    });
+
 });
 
 module.exports = router;
