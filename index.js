045de8a8a940e94c3b74844fff5f45b454ca408f
Robson Carlos <rcs.robson.carlos.santos@gmail.com>
2020-05-05 19:24:45 +0100

    pagination articles

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const connection = require('./database/database');
 const categoriesController = require('./categories/categoriesController');
 const articlesController = require('./articles/articlesController');
 //models
-const Category = require('./categories/category');
-const Article = require('./articles/article'); 
+const modelCategory = require('./categories/category');
+const modelArticle = require('./articles/article');
 
 //view engine
 app.set('view engine', 'ejs');
@@ -16,24 +16,80 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 //body parser
-app.use(bodyParse.urlencoded({ extended: false}));
+app.use(bodyParse.urlencoded({ extended: false }));
 app.use(bodyParse.json());
 
 //Datadase
 connection.authenticate().then(() => {
     console.log('Conexao com sucesso!');
 }).catch((erro) => {
-    console.log('Conexao erro: '+erro);
+    console.log('Conexao erro: ' + erro);
 });
 
 //externs routers config
-app.use('/',categoriesController);
-app.use('/',articlesController);
+app.use('/', categoriesController);
+app.use('/', articlesController);
 
-app.get('/',(req, res) => {
-    res.render('index');
+app.get('/', (req, res) => {
+    var limit = 4; //limit depends on the articlesController.js ('/articles/page/:num')
+    modelArticle.findAll({
+        order: [
+            ['id', 'DESC']
+        ],
+        limit: limit
+    }).then(articles => {
+        modelCategory.findAll().then(categories => {
+            res.render('index', {
+                articles: articles,
+                categories: categories
+            });
+        });
+    });
 });
 
-app.listen('8080',() => {
+app.get('/:slug', (req, res) => {
+    var slug = req.params.slug;
+    modelArticle.findOne({ where: { slug: slug } }).then(article => {
+        if (article != undefined) {
+            modelCategory.findAll().then(categories => {
+                res.render('article', {
+                    article: article,
+                    categories: categories
+                });
+            });
+        } else {
+            res.redirect('/');
+        }
+    }).catch(erro => {
+        res.redirect('/');
+    });
+});
+
+app.get('/category/:slug', (req, res) => {
+    var slug = req.params.slug;
+    modelCategory.findOne(
+        {
+            where:
+                { slug: slug },
+            include: [
+                { model: modelArticle }
+            ]
+        }).then(category => {
+            if (category != undefined) {
+                modelCategory.findAll().then(categories => {
+                    res.render('index', {
+                        articles: category.articles,
+                        categories: categories
+                    });
+                });
+            } else {
+                res.redirect('/');
+            }
+        }).catch(erro => {
+            res.redirect('/');
+        });
+});
+
+app.listen('8080', () => {
     console.log('Servidor rodando!');
 });
